8886182a224874d1a6234cd00bb30686774834c4
lcappucci <44978906+lcappucci@users.noreply.github.com>
2025-02-28 13:25:46 -0500

    First update to ui.ts - General updates to all

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `M&AI`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Custom AI for S-4 Filings`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const FOOTER_MESSAGE: string = `Custom AI for M&A Data`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
+export const PAGE_TITLE: string = `M&AI`;
+export const PAGE_DESCRIPTION: string = `Hey there! My name is ${AI_NAME}, ${OWNER_NAME}'s go-to M&AI Numbers Guy!`;
 
-export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
+export const EMPTY_CITATION_MESSAGE: string = "Source: CapitalIQ";
