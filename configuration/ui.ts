95fe636053ed88052733dbaacb0ba6d844b26a9f
lcappucci <44978906+lcappucci@users.noreply.github.com>
2025-02-28 13:40:22 -0500

    Update ui.ts part 2

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Custom AI for M&A Data`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
 export const PAGE_TITLE: string = `M&AI`;
-export const PAGE_DESCRIPTION: string = `Hey there! My name is ${AI_NAME}, ${OWNER_NAME}'s go-to M&AI Numbers Guy!`;
+export const PAGE_DESCRIPTION: string = `AI Assistant for all things M&A`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Source: CapitalIQ";
